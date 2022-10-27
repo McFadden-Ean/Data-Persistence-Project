615defa8efb035c9ddb656a1203c70e8dc87011c
McFadden-Ean <em574518@ohio.edu>
2022-10-27 17:37:15 -0400

    final version
    Finished doing this project

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,78 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class MainManager : MonoBehaviour
 {
-    public Brick BrickPrefab;
-    public int LineCount = 6;
-    public Rigidbody Ball;
+    public static MainManager Instance;
+    public string playerName;
+    public int highScore;
 
-    public Text ScoreText;
-    public Button returnButton;
-    public GameObject GameOverText;
-    
-    private bool m_Started = false;
-    private int m_Points;
-    
-    private bool m_GameOver = false;
+    public string sessionName;
+    public int sessionScore;
 
-    
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        const float step = 0.6f;
-        int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
-        for (int i = 0; i < LineCount; ++i)
+        if (Instance != null)
         {
-            for (int x = 0; x < perLine; ++x)
-            {
-                Vector3 position = new Vector3(-1.5f + step * x, 2.5f + i * 0.3f, 0);
-                var brick = Instantiate(BrickPrefab, position, Quaternion.identity);
-                brick.PointValue = pointCountArray[i];
-                brick.onDestroyed.AddListener(AddPoint);
-            }
+            Destroy(gameObject);
+            return;
         }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        LoadHighScore();
+    }
+    [System.Serializable]
+
+    class SaveData
+    {
+        public string playerName;
+        public int highScore;
+
     }
 
-    private void Update()
+    public void SaveHighScore(int sessionScore, string sessionName)
     {
-        if (!m_Started)
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                m_Started = true;
-                float randomDirection = Random.Range(-1.0f, 1.0f);
-                Vector3 forceDir = new Vector3(randomDirection, 1, 0);
-                forceDir.Normalize();
+        SaveData data = new SaveData();
+        data.playerName = sessionName;
+        data.highScore = sessionScore;
 
-                Ball.transform.SetParent(null);
-                Ball.AddForce(forceDir * 2.0f, ForceMode.VelocityChange);
-            }
-        }
-        else if (m_GameOver)
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-            }
-        }
+        string json = JsonUtility.ToJson(data);
+
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
     }
 
-    void AddPoint(int point)
+    public void LoadHighScore()
     {
-        m_Points += point;
-        ScoreText.text = $"Score : {m_Points}";
-    }
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
 
-    public void GameOver()
-    {
-        m_GameOver = true;
-        GameOverText.SetActive(true);
-        returnButton.gameObject.SetActive(true);
+            playerName = data.playerName;
+            highScore = data.highScore;
+        }
     }
 }
--- Assets/Scripts/UIMenu.cs
+++ Assets/Scripts/UIMenu.cs
@@ -3,11 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
 public class UIMenu : MonoBehaviour
 {
+
+    public TMP_Text bestScoreText;
+    public string textInput;
+    public TMP_InputField nameInput;
+
+    private void Start()
+    {
+        MainManager.Instance.LoadHighScore();
+
+        if(MainManager.Instance != null)
+        {
+            if (MainManager.Instance.highScore!= 0)
+            {
+                DisplayHighScore();
+            }
+            else
+            {
+                DisplayName();
+            }
+        }
+        else
+        {
+            bestScoreText.text = "Hello, set a high score!";
+        }
+        
+    }
+
+    public void SavePlayerName()
+    {
+        textInput = nameInput.text;
+        MainManager.Instance.sessionName = textInput;
+    }
+
+    void DisplayHighScore()
+    {
+        bestScoreText.text = "The current high score is " + MainManager.Instance.highScore + " set by " + MainManager.Instance.playerName;
+    }
+   
+    void DisplayName()
+    {
+        bestScoreText.text = MainManager.Instance.sessionName + ", set a high score!";
+    }
     public void StartGame()
     {
         SceneManager.LoadScene(1);
@@ -17,6 +60,7 @@ public class UIMenu : MonoBehaviour
     {
         SceneManager.LoadScene(0);
     }
+
     public void Quit()
     {
     #if UNITY_EDITOR
